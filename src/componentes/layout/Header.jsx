b582befb4f0c14e1f2e8a5e3de4b82071a65fb40
tamroqtre <tamara.roquigny@gmail.com>
2026-07-02 19:39:58 -0300

    menu hamburguesa

--- src/componentes/layout/Header.jsx
+++ src/componentes/layout/Header.jsx
@@ -1,23 +1,29 @@
 import styles from './Layout.module.css'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { useCart } from '../../Context/CartContext';
 import CartWidget from '../Cart/CartWidget';
-import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../Context/AuthContext';
+import { useState } from 'react';
 
 function Header() {
 
-    const {user, logout} = useAuth();
+    const { user, logout } = useAuth();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <header className={styles.header}>
             <div className={styles["logo-wrapper"]}>
+                <button className={styles.hamburgerBtn} onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+
                 <Link to="/" className={styles.logoLink}>
-                <h1>Akiba Crew </h1>
-                <img src="/images/logo2.png" alt="Logo Akiba Crew" className={styles["logo-img"]} />
+                    <h1>Akiba Crew </h1>
+                    <img src="/images/logo2.png" alt="Logo Akiba Crew" className={styles["logo-img"]} />
                 </Link>
             </div>
-            <nav className={styles.nav}>
+
+            <nav className={`${styles.nav} ${menuOpen ? styles.navOpen : ''}`}>
 
                 <NavLink to="/" className={({ isActive }) => isActive ? styles["link-activo"] : styles["link-normal"]}>Inicio</NavLink>
 
